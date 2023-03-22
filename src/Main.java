d7755f2f29a44888d313cbac5b312108d9111a5c
sofia_chuba <sofiia.chuba@student.karazin.ua>
2023-03-22 17:30:46 +0200

    Homework

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    static boolean dividesByTwo(int a){
+        return (a%2==0);
+    }
+
+    public static void main(String[] args){
+
+        System.out.println( dividesByTwo((int) 3.9) );
+    }
+}
